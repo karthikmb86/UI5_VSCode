6abd3e0c9bc167a1e9b7cde2e34a1db4c259b3cd
KarthikBangera <karthikmb86@gmail.com>
2021-04-08 22:06:25 +1000

    personal laptop component

---
+++ webapp/component.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+ ], function (UIComponent, JSONModel, ResourceModel) {
+    "use strict";
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+        metadata : {
+            "rootView": {
+               "viewName": "sap.ui.demo.walkthrough.view.App",
+               "type": "XML",
+               "async": true,
+               "id": "app"
+            }
+         }, 
+       init : function () {
+          // call the init function of the parent
+          UIComponent.prototype.init.apply(this, arguments);
+       }
+    });
+ });
